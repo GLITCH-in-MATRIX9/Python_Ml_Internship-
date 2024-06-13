4721cb42f398494c6a0cb76f91568f6062e674d4
Anjali Dass <98489478+GLITCH-in-MATRIX9@users.noreply.github.com>
2024-06-14 00:28:10 +0530

    commit

---
+++ question_1.py
@@ -0,0 +1,12 @@
+def main():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    
+    # Calculate the sum
+    total = num1 + num2
+    
+    # Print the result
+    print("The sum is:", total)
+
+if __name__ == "__main__":
+    main()
